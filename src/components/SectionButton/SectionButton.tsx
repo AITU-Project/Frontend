1e0c41a2b679d553b340535912660f6df5a1f186
R0onen <erni100105@gmail.com>
2024-11-17 12:32:12 +0100

    feat: add section button

---
+++ src/components/SectionButton/SectionButton.tsx
@@ -0,0 +1,23 @@
+import { MouseEventHandler } from 'react';
+
+import { combineStyles } from '../../utils/combineStyleClasses';
+import style from './SectionButton.module.css';
+
+type SectionButtonType = 'yellow' | 'green';
+
+interface SectionButtonProps {
+  title: string;
+  type: SectionButtonType;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export function SectionButton(props: Readonly<SectionButtonProps>) {
+  return (
+    <button
+      className={combineStyles([style.button, style[props.type]])}
+      onClick={props.onClick}
+    >
+      {props.title}
+    </button>
+  );
+}
