b63e319dabae2599c49d913145bb402cb106aa01
R0onen <erni100105@gmail.com>
2024-11-17 12:33:22 +0100

    feat: add studio page logic

--- src/pages/Studio/Studio.tsx
+++ src/pages/Studio/Studio.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import addDocument from '../../assets/images/side-panel/add-document.svg';
 import identification from '../../assets/images/side-panel/identification.svg';
 import menu from '../../assets/images/side-panel/menu.svg';
@@ -9,49 +11,77 @@ import { CreateConclusion } from '../../components/CreateConclusion';
 import { SidePanel, SidePanelTabProps } from '../../components/SidePanel';
 import style from './Studio.module.css';
 
-const sidePanelTabs: SidePanelTabProps[] = [
-  {
-    title: 'Реестр повесток и вызовов',
-    icon: identification,
-    onClick: () => {},
-  },
-  {
-    title: 'Создание заключения',
-    icon: addDocument,
-    onClick: () => {},
-  },
-  {
-    title: 'Журнал заключений',
-    icon: menu,
-    onClick: () => {},
-  },
-  {
-    title: 'Профиль',
-    icon: profile,
-    onClick: () => {},
-  },
-  {
-    title: 'Настройки',
-    icon: settings,
-    onClick: () => {},
-  },
-  {
-    title: 'Помощь',
-    icon: question,
-    onClick: () => {},
-  },
-  {
-    title: 'Вход',
-    icon: signIn,
-    onClick: () => {},
-  },
-];
+const EmptyPage = () => {
+  return <></>;
+};
 
 function Studio() {
+  const [activeTab, setActiveTab] = useState(-1);
+  const [tabContent, setTabContent] = useState(EmptyPage);
+
+  const sidePanelTabs: SidePanelTabProps[] = [
+    {
+      title: 'Реестр повесток и вызовов',
+      icon: identification,
+      onClick: () => {
+        setActiveTab(0);
+        setTabContent(EmptyPage);
+      },
+    },
+    {
+      title: 'Создание заключения',
+      icon: addDocument,
+      onClick: () => {
+        setActiveTab(1);
+        setTabContent(CreateConclusion());
+      },
+    },
+    {
+      title: 'Журнал заключений',
+      icon: menu,
+      onClick: () => {
+        setActiveTab(2);
+        setTabContent(EmptyPage);
+      },
+    },
+    {
+      title: 'Профиль',
+      icon: profile,
+      onClick: () => {
+        setActiveTab(3);
+        setTabContent(EmptyPage);
+      },
+    },
+    {
+      title: 'Настройки',
+      icon: settings,
+      onClick: () => {
+        setActiveTab(4);
+        setTabContent(EmptyPage);
+      },
+    },
+    {
+      title: 'Помощь',
+      icon: question,
+      onClick: () => {
+        setActiveTab(5);
+        setTabContent(EmptyPage);
+      },
+    },
+    {
+      title: 'Вход',
+      icon: signIn,
+      onClick: () => {
+        setActiveTab(6);
+        setTabContent(EmptyPage);
+      },
+    },
+  ];
+
   return (
     <div className={style.main}>
-      <SidePanel tabs={sidePanelTabs} />
-      <CreateConclusion />
+      <SidePanel tabs={sidePanelTabs} active={activeTab} />
+      {tabContent}
     </div>
   );
 }
