0bc8eb5c0437568ad3cb7e7ae91eecbb5472ecb5
R0onen <erni100105@gmail.com>
2024-11-17 15:14:49 +0100

    feat: add min max

--- src/components/SectionInput/SectionInput.tsx
+++ src/components/SectionInput/SectionInput.tsx
@@ -6,6 +6,8 @@ interface SectionInputProps {
   label: string;
   type: SectionInputTypes;
   name: string;
+  min?: string;
+  max?: string;
 }
 
 export function SectionInput(props: Readonly<SectionInputProps>) {
@@ -14,7 +16,24 @@ export function SectionInput(props: Readonly<SectionInputProps>) {
       <label className={style.label} htmlFor={props.name}>
         {props.label}
       </label>
-      <input className={style.input} type={props.type} />
+      <input
+        className={style.input}
+        type={props.type}
+        min={
+          props.min
+            ? props.min
+            : props.type === 'date'
+              ? '1990-01-01'
+              : '1990-01-01T00:00'
+        }
+        max={
+          props.max
+            ? props.max
+            : props.type === 'date'
+              ? '2024-12-31'
+              : '2024-12-31T23:59'
+        }
+      />
     </div>
   );
 }
