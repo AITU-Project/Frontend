50c9d6f5e1d6d3b27ab72a4f80de17db876caeaf
R0onen <erni100105@gmail.com>
2024-11-20 14:34:31 +0100

    fix: update radio button component

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -3,38 +3,26 @@ import { ChangeEventHandler } from 'react';
 import styles from './RadioButton.module.css';
 
 interface RadioButtonProps {
-  label?: string;
+  title?: string;
   name?: string;
   value?: string;
-  notes?: string[];
-  required?: boolean;
-  checked?: boolean;
+
   onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
 export function RadioButton(props: Readonly<RadioButtonProps>) {
   return (
     <div className={styles.container}>
-      <label className={styles.radioLabel}>
+      <label className={styles.radio}>
         <input
           className={styles.radioInput}
           type="radio"
           name={props.name}
           value={props.value}
-          checked={props.checked}
           onChange={props.onChange}
         />
-        {props.label}
+        {props.title}
       </label>
-      {props.notes !== undefined && (
-        <>
-          {props.notes.map((note) => (
-            <span key={note} className={styles.notes}>
-              {note}
-            </span>
-          ))}
-        </>
-      )}
     </div>
   );
 }
