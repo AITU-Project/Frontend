0bdd3dbec9af280243511f25967afe03ec58970c
R0onen <erni100105@gmail.com>
2024-11-20 14:35:04 +0100

    feat: add registration step profile

---
+++ src/components/RegistrationStepProfile/RegistrationStepProfile.tsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+
+import { RegistrationStepProps } from '../../pages/Registration/Registration';
+import { Button } from '../Button';
+import { Input } from '../Input';
+import { RadioButtonGroup } from '../RadioButtonGroup';
+import styles from './RegistrationStepProfile.module.css';
+
+interface ProfileValues {
+  name: string;
+  surname: string;
+  email: string;
+  sex: string;
+  password: string;
+  repeatPassword: string;
+}
+
+export function RegistrationStepProfile(
+  props: Readonly<RegistrationStepProps>
+) {
+  const [values, setValues] = useState({} as ProfileValues);
+
+  const patchValues = (newValues: Partial<ProfileValues>) => {
+    setValues({ ...values, ...newValues });
+  };
+
+  return (
+    <>
+      <div className={styles.formContainer}>
+        <h1 className={styles.title}>Добро пожаловать</h1>
+        <div className={styles.inputsContainer}>
+          <div className={styles.inputsRow}>
+            <Input
+              type="text"
+              name="name"
+              label="Имя"
+              required
+              onChange={(event) => {
+                patchValues({ name: event.target.value });
+              }}
+            />
+            <Input
+              type="text"
+              name="surname"
+              label="Фамилия"
+              required
+              onChange={(event) => {
+                patchValues({ surname: event.target.value });
+              }}
+            />
+          </div>
+          <div className={styles.inputsRow}>
+            <Input
+              type="text"
+              name="email"
+              label="Email"
+              required
+              onChange={(event) => {
+                patchValues({ email: event.target.value });
+              }}
+            />
+            <RadioButtonGroup
+              name="sex"
+              label="Пол"
+              placement="rows"
+              options={[
+                { title: 'Мужской', value: 'male' },
+                { title: 'Женский', value: 'female' },
+              ]}
+              onChange={(event) => {
+                patchValues({ sex: event.target.value });
+              }}
+            />
+          </div>
+          <div className={styles.inputsRow}>
+            <Input
+              type="password"
+              name="password"
+              label="Новый пароль"
+              required
+              onChange={(event) => {
+                patchValues({ password: event.target.value });
+              }}
+            />
+            <Input
+              type="password"
+              name="repeat-password"
+              label="Повторите пароль"
+              required
+              onChange={(event) => {
+                patchValues({ repeatPassword: event.target.value });
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <Button
+        title="Продолжить"
+        onClick={() => {
+          props.patchDto({
+            name: values.name,
+            surname: values.surname,
+            email: values.email,
+            password: values.password,
+            sex: values.sex,
+          });
+          props.nextStep();
+        }}
+      />
+    </>
+  );
+}
