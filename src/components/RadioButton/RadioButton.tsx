294b6049da156d5cddbeddb4b735ccc13beb1ea4
R0onen <erni100105@gmail.com>
2024-11-20 14:13:49 +0500

    feat: creating radio buttons

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -4,25 +4,37 @@ import styles from './RadioButton.module.css';
 
 interface RadioButtonProps {
   label?: string;
-  options: string;
   name?: string;
   value?: string;
   notes?: string[];
   required?: boolean;
+  checked?: boolean;
   onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
 export function RadioButton(props: Readonly<RadioButtonProps>) {
   return (
-    <div>
-      {props.label !== undefined && (
-        <label className={styles.label} htmlFor={props.name}>
-          {props.label}
-        </label>
+    <div className={styles.container}>
+      <label className={styles.radioLabel}>
+        <input
+          className={styles.radioInput}
+          type="radio"
+          name={props.name}
+          value={props.value}
+          checked={props.checked}
+          onChange={props.onChange}
+        />
+        {props.label}
+      </label>
+      {props.notes !== undefined && (
+        <>
+          {props.notes.map((note) => (
+            <span key={note} className={styles.notes}>
+              {note}
+            </span>
+          ))}
+        </>
       )}
-      <input type="radio" name={props.name}>
-        {props.options}
-      </input>
     </div>
   );
 }
