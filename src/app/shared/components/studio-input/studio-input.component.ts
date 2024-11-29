b6e352390331c4458a62176046d7147df0a5b2ce
R0onen <erni100105@gmail.com>
2024-11-29 18:17:58 +0100

    feat: add all changes

--- src/app/shared/components/studio-input/studio-input.component.ts
+++ src/app/shared/components/studio-input/studio-input.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+type InputType = 'text';
 
 @Component({
   selector: 'app-studio-input',
   standalone: true,
   imports: [],
   templateUrl: './studio-input.component.html',
-  styleUrl: './studio-input.component.scss'
+  styleUrl: './studio-input.component.scss',
 })
 export class StudioInputComponent {
-
+  title = input<string>('');
+  type = input<InputType>('text');
+  placeholder = input<string>('');
+  value = input<string>('');
 }
