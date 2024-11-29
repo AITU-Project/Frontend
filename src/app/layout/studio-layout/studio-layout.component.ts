eedcaf2c65e99f561f28d5aac4d149a268961721
R0onen <erni100105@gmail.com>
2024-11-29 16:26:01 +0100

    feat: add side panel

--- src/app/layout/studio-layout/studio-layout.component.ts
+++ src/app/layout/studio-layout/studio-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-studio-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SharedModule],
   templateUrl: './studio-layout.component.html',
   styleUrl: './studio-layout.component.scss',
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthInputComponent } from './components/auth-input/auth-input.component';
 import { AuthButtonComponent } from './components/auth-button/auth-button.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
 
 @NgModule({
-  imports: [CommonModule, AuthInputComponent, AuthButtonComponent],
-  exports: [CommonModule, AuthInputComponent, AuthButtonComponent],
+  imports: [
+    CommonModule,
+    AuthInputComponent,
+    AuthButtonComponent,
+    SidePanelComponent,
+  ],
+  exports: [
+    CommonModule,
+    AuthInputComponent,
+    AuthButtonComponent,
+    SidePanelComponent,
+  ],
 })
 export class SharedModule {}
