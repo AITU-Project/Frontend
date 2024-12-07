73f4dcbf7c033685b505de3f147bfa31759fc561
R0onen <erni100105@gmail.com>
2024-12-07 05:37:47 +0100

    fix: manage imports

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './page/sign-in/sign-in.component';
 import { SignUpComponent } from './page/sign-up/sign-up.component';
 import { AuthRoutingModule } from './auth.routing';
-import { SharedModule } from '../../shared/shared.module';
 import { VerifyComponent } from './page/verify/verify.component';
 
 @NgModule({
   imports: [
     SignInComponent,
     SignUpComponent,
-    AuthRoutingModule,
     VerifyComponent,
-    SharedModule,
+    AuthRoutingModule,
   ],
 })
 export class AuthModule {}
