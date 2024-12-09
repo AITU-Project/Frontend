9b7d8070724b0e50f1a2bf0691d437cbdc733e1d
R0onen <erni100105@gmail.com>
2024-12-09 20:27:55 +0100

    fix: some code refactoring

--- src/app/shared/components/studio-input/studio-input.component.ts
+++ src/app/shared/components/studio-input/studio-input.component.ts
@@ -1,6 +1,5 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { StudioInputDirective } from '../../directives/studio-input.directive';
-import { Contains } from '../../directives/auth-input.directive';
 
 type InputType = 'text' | 'date' | 'datetime-local';
 
@@ -14,12 +13,4 @@ type InputType = 'text' | 'date' | 'datetime-local';
 export class StudioInputComponent extends StudioInputDirective {
   type = input<InputType>('text');
   placeholder = input<string>('');
-  name = input<string>('');
-
-  contains = output<Contains<never>>();
-
-  interaction(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.contains.emit({ name: this.name() as never, value: element.value });
-  }
 }
--- src/app/shared/directives/studio-input.directive.ts
+++ src/app/shared/directives/studio-input.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, input } from '@angular/core';
+import { Directive, input, output } from '@angular/core';
+import { Contains } from './auth-input.directive';
 
 @Directive({
   standalone: true,
@@ -6,4 +7,12 @@ import { Directive, input } from '@angular/core';
 export class StudioInputDirective {
   title = input<string>('');
   value = input<string>('');
+  name = input<string>('');
+
+  contains = output<Contains<never>>();
+
+  interaction(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.contains.emit({ name: this.name() as never, value: element.value });
+  }
 }
