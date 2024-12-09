c985ae939c86f97192bda3b6a61dd2e8b77568d0
R0onen <erni100105@gmail.com>
2024-12-09 20:35:32 +0100

    feat: update profile page

--- src/app/modules/studio/page/profile/profile.component.ts
+++ src/app/modules/studio/page/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from '../../../../shared/shared.module';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,4 +10,32 @@ import { SharedModule } from '../../../../shared/shared.module';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  private readonly auth = inject(AuthService);
+
+  constructor() {
+    this.auth.profile()?.subscribe({
+      next: (response: any) => {
+        const profile = response.profile;
+        this.form = new FormGroup({
+          name: new FormControl(profile.name),
+          surname: new FormControl(profile.surname),
+          email: new FormControl(profile.email),
+          region: new FormControl(profile.region),
+          role: new FormControl(profile.role),
+        });
+      },
+      error: (errors) => {
+        console.log(errors);
+      },
+    });
+  }
+
+  public form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    email: new FormControl(''),
+    region: new FormControl(''),
+    role: new FormControl(''),
+  });
+}
